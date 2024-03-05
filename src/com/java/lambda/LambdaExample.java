802879d53a5c1424f394dda4eee8abc5bfdcab4c
Khomendra <khomendrakumar.ak.94@gmail.com>
2024-03-05 14:43:40 +0530

    more optimized code to pass lambda exp as function parameter

--- src/com/java/lambda/LambdaExample.java
+++ src/com/java/lambda/LambdaExample.java
@@ -8,19 +8,9 @@ interface Shape{
 
 public class LambdaExample {
     public static void main(String []args){
-
-        //Java 8 features to make overridden method
-        Shape rectangle = () -> System.out.println("Rectangle class: draw() method");
-
-
-        Shape square = () -> System.out.println("Square class: draw() method");
-
-
-        Shape circle = () -> System.out.println("Circle class: draw() method");
-
-        print(rectangle);
-        print(square);
-        print(circle);
+        print(() -> System.out.println("Rectangle class: draw() method"));
+        print(() -> System.out.println("Square class: draw() method"));
+        print(() -> System.out.println("Circle class: draw() method"));
 
     }
 
